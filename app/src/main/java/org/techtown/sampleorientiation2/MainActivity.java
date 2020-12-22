e572eba77878bd1b311628a560cd27f793ccedac
Beaksooyeol <merrygoaround0726@gmail.com>
2020-12-22 14:42:22 +0900

    SampleOrientation end

--- app/src/main/java/org/techtown/sampleorientiation2/MainActivity.java
+++ app/src/main/java/org/techtown/sampleorientiation2/MainActivity.java
@@ -2,7 +2,9 @@ package org.techtown.sampleorientiation2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            showToast("방향 : ORIENTATION_LANDSCAPE");
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            showToast("방향 : ORIENTATION_PORTRAIT");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showToast("onStart 호출");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showToast("onStop 호출");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy 호출");
+    }
+
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
     }
 }
